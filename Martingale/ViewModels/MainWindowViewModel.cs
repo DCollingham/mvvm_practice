97a1469364f019cd39051305fe360939cf78a192
NiceLuck <40005211@petroc.ac.uk>
2022-06-13 16:11:11 +0100

    Add project files.

---
+++ Martingale/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Martingale.Models;
+
+namespace Martingale.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private User user;
+        private int _id;
+        private string _name;
+        private decimal _bankroll;
+
+        public int Id
+        {
+            get => user.Id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get => user.Name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public decimal Bankroll
+        {
+            get => user.BankRoll;
+            set 
+            { 
+                _bankroll = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            user = new User
+            {
+                Id = 1,
+                Name = "Dylan",
+                BankRoll = 10
+            };
+        }
+    }
+}
